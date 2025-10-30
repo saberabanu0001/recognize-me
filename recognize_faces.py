539e8b1039b2b9fc1f709c6c53857f23c9d14103
Sabera Banu <saberabanu0001@gmail.com>
2025-10-30 14:01:09 +0900

    aaded the known and unknown face logic

--- recognize_faces.py
+++ recognize_faces.py
@@ -7,9 +7,35 @@ known_faces_directory = "/Users/saberabanu/All Drives/Personal/face-recognition/
 unknown_faces_directory = "/Users/saberabanu/All Drives/Personal/face-recognition/unknown-faces"
 
 known_faces = []
-unknown_faces = []
+known_names = []
 
 for filename in os.listdir(known_faces_directory):
     images = face_recognition.load_image_files(f"known_faces_directory/{known_faces}")
+    encoding = face_recognition.face_encodings(images)[0]
+    known_faces.append(encoding)
+    known_faces.append(os.path.splitext(filename)[0])
+
+    print("Processing known faces.........")
+
+for filename in os.listdir(known_faces_directory):
+    print("Checking filename....")
+    images = face_recognition.load_image_files(f"known_faces_directory/{known_faces}")
+    locations = face_recognition.face_locations(images)[0]
+    encodings = face_recognition.face_encodings(images, locations)
 
 
+
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+for face_encoding, face_location in zip(encodings, locations):
+    results = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.6)
+
+    name = "unknown"
+
+    if True in results:
+        match_index = results.index[True]
+        name = known_names[match_index]
+
+    #draw boxes
+    top, right, bottom, left = face_location
+    
